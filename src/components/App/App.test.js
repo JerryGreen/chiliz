1772a70d48fba40f2648a9bbab0e44370c66ec71
Jerry Green <jerrygreenest@gmail.com>
2019-03-27 00:37:35 +0500

    fixed test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import { h, render } from 'preact'
-// import App from './App'
+import App from './App'
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+global.localStorage = localStorageMock
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div')
-  // render(<App />, div)
+  const div = document.createElement('div')
+  render(<App />, div)
 })
